96a73726f25d81a34b32a68c7e85077d999602a3
msc <2318700141@qq.com>
2020-02-26 16:36:49 +0800

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,4 @@
 import 'package:flutter/material.dart';
-import './components/recent_hot.dart' ;
+import 'app.dart'
 void main() => runApp(MyApp());
-class MyApp extends StatelessWidget{
-  @override
-//  Widget build(BuildContext context){
-//    return Scaffold(
-//      body:ListView(
-//        children:<Widget>[
-//          RecentHot()
-//        ]
-//      )
-//    );
-//  }
-//}
 
