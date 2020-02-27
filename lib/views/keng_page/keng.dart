0fb821c538f5594f755b3864e966b5e212bfeaf4
msc <2318700141@qq.com>
2020-02-27 21:27:02 +0800

    11

--- lib/views/keng_page/keng.dart
+++ lib/views/keng_page/keng.dart
@@ -18,157 +18,59 @@ class KengContent extends StatelessWidget{
     return Column(
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.symmetric(horizontal:10),
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(
-                  Icons.close,
-                  color: Colors.grey,
-                  size: 28,
-                ),
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.close,
-                  color:Colors.black,
-                  size:20,
-                ), 
-                onPressed: (){
-                  Navigator.pop(context);
-                }
-              )
-            ],)
-        ),
-        Container(
-          padding: EdgeInsets.only(left:22),
-          alignment: Alignment.centerLeft,
-          child:Text(
-            '配送至',
-            style:TextStyle(
-              fontSize: 16,
-              color: Colors.black45,
-              fontWeight: FontWeight.w500,
-              letterSpacing: 1
-            ),
-          )
-        ),
-        Padding(
           padding: EdgeInsets.symmetric(horizontal:22),
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Icon(
-                null,
-                size:15
+            child: Container(
+              height: 50,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                boxShadow:[
+                  BoxShadow(
+                    color:Colors.grey[300],
+                    blurRadius: 3,
+                    spreadRadius: 0.2,
+                  )
+                ],
+                color:Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
               ),
-              Text(
-                '当前位置',
-                style:TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 1
-                )
-              )
-            ],
-          )
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal:22),
-          child:Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Flexible(
-                flex: 9,
-                child: Container(
-                  height: 50,
-                  alignment: Alignment.centerLeft,
-                  decoration: BoxDecoration(
-                    boxShadow:[
-                      BoxShadow(
-                        color:Colors.grey[300],
-                        blurRadius: 3,
-                        spreadRadius: 0.2,
-                      )
-                    ],
-                    color:Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child:TextFormField(
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 18,
-                    ),
-                    cursorColor: Colors.black54,
-                    decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.transparent,
-                        )
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color:Colors.transparent,
-                        ),
-                      ),
-                      hintText: '您想点些什么?',
-                      helperStyle: TextStyle(
-                        color:Colors.black38,
-                        fontSize: 18
-                      ),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        size:18,
-                        color:Colors.black54,
-                      )
-                    ),
-                  ),
+              child:TextFormField(
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18,
                 ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey[300],
-                        blurRadius: 3,
-                        spreadRadius: 0.2
-                      )
-                    ],
-                    color:Colors.white,
-                    borderRadius: BorderRadius.circular(10),
+                cursorColor: Colors.black54,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    )
                   ),
-                  child:IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.close,
-                      color: Colors.grey,
-                      size: 28,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color:Colors.transparent,
                     ),
+                  ),
+                  hintText: '输入坑或组织名称',
+                  helperStyle: TextStyle(
+                    color:Colors.black38,
+                    fontSize: 18
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size:18,
+                    color:Colors.black54,
                   )
                 ),
-              )
-            ],
-          )
+              ),
+            ),
         ),
         Padding(
-          padding: EdgeInsets.only(
-            left:22,top:20
-          ),
+          padding: EdgeInsets.only(top:20),
           child: Text(
-            '甜品分类',
+            '近期热门',
             style:TextStyle(
-              fontSize: 20,
+              fontSize: 30,
               color:Colors.black,
-              fontWeight: FontWeight.w700,
               letterSpacing: 1
             )
           ),
