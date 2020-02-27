acfc4aa22dc3030acd8d952014666bed9a57efa7
msc <2318700141@qq.com>
2020-02-27 21:07:20 +0800

    10

--- lib/views/keng_page/keng.dart
+++ lib/views/keng_page/keng.dart
@@ -77,6 +77,111 @@ class KengContent extends StatelessWidget{
               )
             ],
           )
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal:22),
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Flexible(
+                flex: 9,
+                child: Container(
+                  height: 50,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    boxShadow:[
+                      BoxShadow(
+                        color:Colors.grey[300],
+                        blurRadius: 3,
+                        spreadRadius: 0.2,
+                      )
+                    ],
+                    color:Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child:TextFormField(
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                    cursorColor: Colors.black54,
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                        )
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color:Colors.transparent,
+                        ),
+                      ),
+                      hintText: '您想点些什么?',
+                      helperStyle: TextStyle(
+                        color:Colors.black38,
+                        fontSize: 18
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size:18,
+                        color:Colors.black54,
+                      )
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[300],
+                        blurRadius: 3,
+                        spreadRadius: 0.2
+                      )
+                    ],
+                    color:Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child:IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.close,
+                      color: Colors.grey,
+                      size: 28,
+                    ),
+                  )
+                ),
+              )
+            ],
+          )
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            left:22,top:20
+          ),
+          child: Text(
+            '甜品分类',
+            style:TextStyle(
+              fontSize: 20,
+              color:Colors.black,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 1
+            )
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left:22),
+          height:100,
+          child:ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context,int index){
+              return ;
+            }
+          )
         )
       ],
     );
