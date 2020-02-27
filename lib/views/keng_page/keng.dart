5fa038098772cad01d358e8f47bd907c99710883
msc <2318700141@qq.com>
2020-02-27 16:26:30 +0800

    5

--- lib/views/keng_page/keng.dart
+++ lib/views/keng_page/keng.dart
@@ -7,35 +7,51 @@ class KengState extends State<KengPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Refresh(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10, top: 10),
-              child: Text("推荐话题"),
-            ),
-          ),
-          SliverFixedExtentList(
-            itemExtent: 130.0,
-            delegate:
-                SliverChildBuilderDelegate((BuildContext context, int index) {
-              //创建列表项
-              return Swiper(
-                pagination: SwiperPagination(),
-                itemBuilder: _swiperBuilder,
-                itemCount: swiperItemCount,
-                scrollDirection: Axis.horizontal,
-//                autoplay: true,
-                onTap: _goOtherApp,
-              );
-            }, childCount: 1), //50个列表项
-          ),
-          //PostList(postPage.list),
-        ],
-      ),
+      body:KengContent()
     );
   }
 }
-
-
+class KengContent extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 10, top: 10),
+          child: Text("推荐话题"),
+        ),
+        Container(
+            constraints: BoxConstraints(
+              minWidth: 100,
+              minHeight: 200
+            ),
+          ),
+          Text(
+            "we",
+            softWrap: false,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          ),
+          Row(
+            children: <Widget>[
+              Image.network(
+                "wqd",
+                width: 20,
+                height: 20,
+              ),
+              Text("3"),
+              Expanded(
+                child: SizedBox(),
+              ),
+              LikeButton(
+                direction: Axis.horizontal,
+                num: 1,
+              ),
+            ],
+          ),
+        ],
+      );
+  }
+}
 
