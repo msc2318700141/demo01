dbb60c6d2653855b3c13b9ddbc319c2c0e5452d1
msc <2318700141@qq.com>
2020-02-27 21:31:21 +0800

    12

--- lib/views/keng_page/keng.dart
+++ lib/views/keng_page/keng.dart
@@ -19,58 +19,95 @@ class KengContent extends StatelessWidget{
       children: <Widget>[
         Padding(
           padding: EdgeInsets.symmetric(horizontal:22),
-            child: Container(
-              height: 50,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                boxShadow:[
-                  BoxShadow(
-                    color:Colors.grey[300],
-                    blurRadius: 3,
-                    spreadRadius: 0.2,
-                  )
-                ],
-                color:Colors.grey[200],
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child:TextFormField(
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 18,
-                ),
-                cursorColor: Colors.black54,
-                decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.transparent,
-                    )
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Flexible(
+                flex: 9,
+                child: Container(
+                  height: 50,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    boxShadow:[
+                      BoxShadow(
+                        color:Colors.grey[300],
+                        blurRadius: 3,
+                        spreadRadius: 0.2,
+                      )
+                    ],
+                    color:Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color:Colors.transparent,
+                  child:TextFormField(
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                    cursorColor: Colors.black54,
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                        )
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color:Colors.transparent,
+                        ),
+                      ),
+                      hintText: '输入坑或组织名称',
+                      helperStyle: TextStyle(
+                        color:Colors.black38,
+                        fontSize: 18
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size:18,
+                        color:Colors.black54,
+                      )
                     ),
                   ),
-                  hintText: '输入坑或组织名称',
-                  helperStyle: TextStyle(
-                    color:Colors.black38,
-                    fontSize: 18
-                  ),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    size:18,
-                    color:Colors.black54,
-                  )
                 ),
               ),
-            ),
+              Flexible(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[300],
+                        blurRadius: 3,
+                        spreadRadius: 0.2
+                      )
+                    ],
+                    color:Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child:IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.close,
+                      color: Colors.grey,
+                      size: 28,
+                    ),
+                  )
+                ),
+              )
+            ],
+          )
         ),
         Padding(
-          padding: EdgeInsets.only(top:20),
+          padding: EdgeInsets.only(
+            left:22,top:20
+          ),
           child: Text(
-            '近期热门',
+            '甜品分类',
             style:TextStyle(
-              fontSize: 30,
+              fontSize: 20,
               color:Colors.black,
+              fontWeight: FontWeight.w700,
               letterSpacing: 1
             )
           ),
